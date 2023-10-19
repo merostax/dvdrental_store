192b08c0c9ce3bd7497d16ce04b566be6492236e
kt <kt@ciphron.de>
2023-10-19 17:04:11 +0200

    fix configurations in store

--- src/main/java/ApplicationConfig.java
+++ src/main/java/ApplicationConfig.java
@@ -1,7 +1,14 @@
 import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.Application;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
-@ApplicationPath("/api")
+import java.util.HashSet;
+import java.util.Set;
+
+@ApplicationPath("/")
 public class ApplicationConfig extends Application {
 
 }
