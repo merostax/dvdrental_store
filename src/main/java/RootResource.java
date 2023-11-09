4c1d593e3d00b09e9ad2e59695ba1295bee07b26
kt <kt@ciphron.de>
2023-11-10 00:11:32 +0100

    add hrefs to root api

--- src/main/java/RootResource.java
+++ src/main/java/RootResource.java
@@ -12,8 +12,11 @@ import services.RentalService;
 import services.StaffService;
 import services.StoreService;
 
+import javax.swing.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Path("/")
 public class RootResource {
@@ -24,27 +27,37 @@ public class RootResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response listEndpoints() {
-        List<String> endpoints = new ArrayList<>();
+        List<Map<String, String>> endpoints = new ArrayList<>();
+
+        if (InventoryService.class.isAnnotationPresent(Path.class)) {
+            Path pathAnnotation = InventoryService.class.getAnnotation(Path.class);
+            endpoints.add(createEndpointMap(uriInfo.getBaseUri() + pathAnnotation.value()));
+        }
+
+        if (RentalService.class.isAnnotationPresent(Path.class)) {
+            Path pathAnnotation = RentalService.class.getAnnotation(Path.class);
+            endpoints.add(createEndpointMap(uriInfo.getBaseUri() + pathAnnotation.value()));
+        }
+
+        if (StaffService.class.isAnnotationPresent(Path.class)) {
+            Path pathAnnotation = StaffService.class.getAnnotation(Path.class);
+            endpoints.add(createEndpointMap(uriInfo.getBaseUri() + pathAnnotation.value()));
+        }
 
         if (StoreService.class.isAnnotationPresent(Path.class)) {
             Path pathAnnotation = StoreService.class.getAnnotation(Path.class);
-            endpoints.add(uriInfo.getBaseUri() + pathAnnotation.value());
-        }  if (RentalService.class.isAnnotationPresent(Path.class)) {
-            Path pathAnnotation = RentalService.class.getAnnotation(Path.class);
-            endpoints.add(uriInfo.getBaseUri() + pathAnnotation.value());
-        }
-        if (StaffService.class.isAnnotationPresent(Path.class)) {
-            Path pathAnnotation = StaffService.class.getAnnotation(Path.class);
-            endpoints.add(uriInfo.getBaseUri() + pathAnnotation.value());
-        }
-        if (InventoryService.class.isAnnotationPresent(Path.class)) {
-            Path pathAnnotation = InventoryService.class.getAnnotation(Path.class);
-            endpoints.add(uriInfo.getBaseUri() + pathAnnotation.value());
+            endpoints.add(createEndpointMap(uriInfo.getBaseUri() + pathAnnotation.value()));
         }
 
         Jsonb jsonb = JsonbBuilder.create();
         String jsonEndpoints = jsonb.toJson(endpoints);
 
         return Response.ok(jsonEndpoints).build();
+    }
+
+    private Map<String, String> createEndpointMap(String href) {
+        Map<String, String> endpointMap = new HashMap<>();
+        endpointMap.put("href", href);
+        return endpointMap;
     }
 }
